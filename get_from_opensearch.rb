9f8329e5c7e91d2789009506d5292fca045b3abe
Emiko TAMIYA <emiko.tamiya@gmail.com>
2012-09-26 18:06:44 +0900

    modified

--- get_from_opensearch.rb
+++ get_from_opensearch.rb
@@ -1,21 +1,19 @@
 require 'open-uri'
 
 p "start #{Time.now}"
-i = 0
-until i > 2
-  num = 2007 - i
-  1.upto(12) do |month|
-    100.times do |index|
-      url = "http://iss.ndl.go.jp/api/opensearch?until=#{num}-#{month.to_s.rjust(2,'0')}-30&from=#{num}-#{month.to_s.rjust(2,'0')}-01&cnt=500&mediatype=1&idx=#{index+1}"
-      os = open(url)
-      datas = os.read
-      File.open("import/#{num}/datas#{month}_#{index}.xml", "w") do |f|
-        f.print datas
-        p "created #{num}#{month}_#{index} #{Time.now}"
-      end
+year = ARGV[0]
+1.upto(12) do |month|
+  50.times do |index|
+    url = "http://iss.ndl.go.jp/api/opensearch?until=#{year}-#{month.to_s.rjust(2,'0')}-30&from=#{year}-#{month.to_s.rjust(2,'0')}-01&cnt=500&mediatype[]=1&idx=#{index+1}&op_id=1"
+    os = open(url)
+    datas = os.read
+    dir = "import/#{year}/"
+    FileUtils.mkdir_p(dir) unless FileTest.exist?(dir)
+    File.open("#{dir}datas#{month}_#{index}.xml", "w") do |f|
+      f.print datas
+      p "created #{year}#{month}_#{index} #{Time.now}"
     end
   end
-  i += 1
 end
 p "end #{Time.now}"
 
